535f0ef40fa8d22d28fffcb537f6de09a464c776
Easwaran Ramamurthy <easwaran6192@gmail.com>
2017-05-02 12:16:22 -0400

    added script for testing mutations

---
+++ GM_JUNB_run_4/test_seqgl_mut_seqs.py
@@ -0,0 +1,64 @@
+from Bio import SeqIO
+import sys
+
+def generate_wildcard_kmers(kmer):
+	wc_kmers = []
+	
+	for i in xrange(len(kmer)):
+		new_kmer = kmer[0:i] + "N" + kmer[i+1:]
+		wc_kmers.append(new_kmer)
+	
+	for i in xrange(len(kmer)-1):
+		new_kmer = kmer[0:i] + "NN" + kmer[i+2:]
+		wc_kmers.append(new_kmer)
+	
+	return wc_kmers
+	
+def predict_label_given_seq(seq):
+	score = 0
+	k = 8
+	for i in xrange(len(seq)-k+1):
+		kmer = seq[i:i+k]
+		if kmer in weights:
+			score+=weights[kmer]
+		
+		for wc_kmer in generate_wildcard_kmers(kmer):
+			if wc_kmer in weights:
+				score+=weights[wc_kmer]
+
+	return 1 if score>0 else -1
+
+def aggregateCountsForPlot(record, orig_seq_id):
+	return None
+	
+if __name__=="__main__":
+	w_file = sys.argv[1]
+	orig_seqs_file = sys.argv[2]
+	mut_seqs_file = sys.argv[3]
+	
+	weights = dict()
+	with open(w_file, 'r') as f:
+		for line in f:
+			splits = line.strip().split()
+			kmer = splits[0]
+			weight = float(splits[1])
+			weights[kmer] = weight
+		
+	print "Predicting labels for orig. sequences"
+	orig_seq_labels = dict()
+	orig_seq_label_file = open("seql_orig_seq_labels.txt", 'w')
+	with open(orig_seqs_file, "rU") as handle:
+		for record in SeqIO.parse(handle, "fasta"):
+			orig_seq_label = predict_label_given_seq(record.seq)
+			orig_seq_label_file.write(record.id+"\t"+str(orig_seq_label)+"\n")
+	orig_seq_label_file.close()		
+	
+	print "Predicting labels for mut. sequences"
+	
+	mut_seq_label_file = open("seql_mut_seq_labels.txt", 'w')	
+	with open(mut_seqs_file, "rU") as handle:
+		for record in SeqIO.parse(handle, "fasta"):
+			mut_seq_label = predict_label_given_seq(record.seq)
+			mut_seq_label_file.write(record.id+"\t"+str(mut_seq_label)+"\n")
+			
+	mut_seq_label_file.close()			
